0006914a2c2488d5e233b7d38c59d6c199f673d5
anormalpersonBE <anormalpersonBE>
2024-03-15 09:42:00 +0000

    Test with pyton

---
+++ Untitled Notebook 2024-03-15 10_34_18.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC select * from gold.calendar;
+# MAGIC
+# MAGIC select * from gold.calendar where yearmonth = 202402;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from gold.calendar where yearmonth = 202102;
--- Untitled Notebook 2024-03-15 10_34_18.sql
+++
@@ -1,8 +0,0 @@
--- Databricks notebook source
-select * from gold.calendar;
-
-select * from gold.calendar where yearmonth = 202402;
-
--- COMMAND ----------
-
-select * from gold.calendar where yearmonth = 202102;
